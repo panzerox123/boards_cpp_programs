5cf93330f2e788982dde24ab8e5ca747ec5ee53e
panzerox123 <kunal.bhat2001@gmail.com>
2019-02-07 19:03:46 +0530

    stacks

--- array_stack.cpp
+++ array_stack.cpp
@@ -8,6 +8,38 @@ void pop();
 void display();
 void mainMenu();
 
+int stack[50];
+int s,top;
+
+void push(){
+    system("clear");
+    cout << "Enter Element: ";
+    int ele;
+    cin >> ele;
+    system("clear");
+    if(top<s){
+        stack[top] = ele;
+        top++;
+    } else {
+        cout << "Error. Stack overflow." << endl;
+    }
+    display();
+    mainMenu();
+}
+
+void pop(){
+    system("clear");
+    if(top > 0) top--; else cout << "Error. Stack underflow." << endl; 
+    display();
+    mainMenu();
+}
+
+void display(){
+    for(int i=top-1; i>=0;i--){
+        cout << stack[i] << endl;
+    }
+}
+
 void mainMenu(){
     cout << "Array Stack Program" << endl;
     cout << "1. Push 2. Pop 3. Exit\n";
@@ -29,5 +61,10 @@ void mainMenu(){
 }
 
 int main(){
+    cout << "Enter stack size . . . : " << endl;
+    cin >> s;
+    top = 0;
+    system("clear");
+    mainMenu();
     return 1;
 }
